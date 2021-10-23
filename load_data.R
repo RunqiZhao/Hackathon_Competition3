c35002f9b1d10d427d1eb66b7de132bb889a507a
RunqiZhao <69561314+RunqiZhao@users.noreply.github.com>
2021-10-23 03:31:02 -0400

    datanew

--- load_data.R
+++ load_data.R
@@ -4,17 +4,28 @@ main <- read.csv("ba_main.csv")
 certs <- read.csv("ba_certs.csv")
 skills <- read.csv("ba_skills.csv")
 main <- main[,c("BGTJobId", "CleanTitle", "CanonTitle", "Specialty", "Employer", "SectorName", "City", "State", "County", "FIPSState", "FIPSCounty", "FIPS", "Lat", "Lon", "Degree", "MaxDegree", "Exp", "MaxExp", "MinSalary", "MaxSalary", "MinHrlySalary", "MaxHrlySalary", "PayFrequency", "SalaryType", "JobHours", "TaxTerm")]
-# key BGTJobId
-job <- merge(main, certs, by.x = "BGTJobId", by.y = "BGTJobId", all.x = TRUE, all.y = TRUE)
-job <- merge(job, skills, by.x = "BGTJobId", by.y = "BGTJobId", all.x = TRUE, all.y = TRUE)
+main <- filter(main, CanonTitle %in% c("Business Analyst","Management Analyst"))
 
-job <- filter(job, CanonTitle %in% c("Business Analyst","Management Analyst"))
-
-job[job == "na"] <- NA
-
+main[main == "na"] <- NA
 # remove na in State
-job <- filter(job, !is.na(State))
+main <- filter(main, !is.na(State))
 
+# key BGTJobId
+job_cet <- merge(main, certs, by.x = "BGTJobId", by.y = "BGTJobId", all.x = TRUE, all.y = TRUE)
+job_skill <- merge(main, skills, by.x = "BGTJobId", by.y = "BGTJobId", all.x = TRUE, all.y = TRUE)
 
+save(job_cet, file ="job_cet.rData")
+save(job_skill, file ="job_skill.rData")
 
-save(job, file ="combined_data.rData")
+# Index
+main$certsIdx <- ifelse(main$BGTJobId %in% unique(certs$BGTJobId), 1, 0)
+job <- main
+# Add Certificate Index
+job$CetfIndex <- ifelse(is.na(job$Degree) & is.na(job$MaxDegree) & !is.na(job$certsIdx), "C",
+                        ifelse(job$Degree == "Bachelor's" & is.na(job$MaxDegree) & is.na(job$certsIdx), "A",
+                               ifelse(job$Degree == "Master's" & is.na(job$MaxDegree) & is.na(job$certsIdx), "B",
+                                      ifelse(job$Degree == "Bachelor's" & is.na(job$MaxDegree) & !is.na(job$certsIdx),"AC",
+                                             ifelse(job$Degree == "Master's" & is.na(job$MaxDegree) & !is.na(job$certsIdx),"BC",
+                                                    ifelse(job$Degree == "Bachelor's" & job$MaxDegree == "Master's" & is.na(job$certsIdx),"AB", NA))))))
+
+save(job, file ="job.rData")
